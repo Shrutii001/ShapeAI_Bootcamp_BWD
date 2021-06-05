5cb7e07328151d4964e4dafae715bf20831c70f4
Shrutii001 <85401135+Shrutii001@users.noreply.github.com>
2021-06-05 19:25:55 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note" >
+      <h1>Reviews from the Javascript and React.js seminar</h1>
+      <p>
+        It was a easy project from Shaurya Sir. He helped us understand everything properly! Kudos to him!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
